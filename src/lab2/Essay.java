ae426c831494bf6233c04368020a0fc7834d7d48
Alex Huang <2476382@edu.vaniercollege.qc.ca>
2025-08-29 13:03:02 -0400

    Implement Essay

---
+++ src/lab2/Essay.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab2;
+
+/**
+ *
+ * @author 2476382
+ */
+public class Essay extends GradedActivity {
+    private double grammar;
+    private double spelling;
+    private double correctLength;
+    private double content;
+
+    public void setScore(double gr, double sp, double len, double cnt) {
+        super.setScore(30 * gr + 20 * sp + 20 * len + 30 * cnt);
+    }
+
+    public double getGrammar() {
+        return grammar;
+    }
+
+    public void setGrammar(double grammar) {
+        this.grammar = grammar;
+    }
+
+    public double getSpelling() {
+        return spelling;
+    }
+
+    public void setSpelling(double spelling) {
+        this.spelling = spelling;
+    }
+
+    public double getCorrectLength() {
+        return correctLength;
+    }
+
+    public void setCorrectLength(double correctLength) {
+        this.correctLength = correctLength;
+    }
+
+    public double getContent() {
+        return content;
+    }
+
+    public void setContent(double content) {
+        this.content = content;
+    }
+}
